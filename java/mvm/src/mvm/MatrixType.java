23b3e17f8b05b99e49be2bc75e531123377f4229
Paul Griffioen <Paul.Griffioen@cwi.nl>
2011-09-02 13:16:05 +0000

    Clean version of mvm package

---
+++ java/mvm/src/mvm/MatrixType.java
@@ -0,0 +1,100 @@
+package mvm;
+
+import units.PowerProduct;
+import units.Unit;
+
+
+public class MatrixType {
+	
+	private Unit factor;
+	private IndexType rowType;
+	private IndexType columnType;
+	
+	public MatrixType(Unit factor, IndexType rowType, IndexType columnType){
+		this.factor = factor;
+		this.rowType = rowType;
+		this.columnType = columnType;
+	}
+	
+	public Unit getFactor() {
+		return factor;
+	}
+
+	public int hashCode() {
+		return factor.hashCode();
+	}
+	
+	public boolean equals(Object other) {
+		if (other == this) {
+			return true;
+		}
+		if (! (other instanceof MatrixType)) {
+			return false;
+		}
+		MatrixType otherType = (MatrixType) other;
+		if (! factor.equals(otherType.factor)) {
+			return false;
+		}
+		if (! rowType.equals(otherType.rowType)) {
+			return false;
+		}
+		if (! columnType.equals(otherType.columnType)) {
+			return false;
+		}
+		return true;
+	}
+
+	public boolean unitSquare() {
+		return rowType.equals(columnType);
+	}
+
+	public MatrixType transpose() {
+		return new MatrixType(factor, columnType.reciprocal(), rowType.reciprocal());
+	}
+
+	public boolean multiplyable(MatrixType other) {
+		return (rowType.multiplyable(other.rowType) && columnType.multiplyable(other.columnType));
+	}
+
+	public MatrixType multiply(MatrixType other){
+		return new MatrixType(factor.multiply(other.factor), rowType.multiply(other.rowType), columnType.multiply(other.columnType));
+	} 
+
+	public MatrixType reciprocal(){
+		return new MatrixType(factor.raise(-1), rowType.reciprocal(), columnType.reciprocal());
+	}
+
+	public boolean joinable(MatrixType other) {
+		return columnType.equals(other.rowType);
+	}
+	
+	public MatrixType join(MatrixType other){
+		return new MatrixType(factor.multiply(other.factor), rowType, other.columnType);
+	}
+	
+	public String pprint(){
+		
+		String factorString = factor.pprint();
+		String rowString = rowType.pprint();
+		String columnString = columnType.pprint();
+		Unit one = new PowerProduct();
+		
+		// Dit kan beter :)
+		String tail= "";
+		if (! columnString.equals("empty")) {
+			tail = " per " + columnString; 
+		}
+		if (! rowString.equals("empty")) {
+			tail = rowString + tail; 
+		}
+		if (factor.equals(one) && tail == "") {
+			return "1";
+		} else if (factor.equals(one)) {
+			return tail;
+		} else if (rowString.equals("empty")) {
+			return factorString + tail;
+		} else {
+			return factorString + " x " + tail;
+		}
+	}
+}
